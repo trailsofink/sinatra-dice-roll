dc8e4e8372e692696135bc818e0d16c82a273a45
trailsofink <trailsofink@gmail.com>
2025-03-01 11:19:31 -0600

    dice rolls complete

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# /Gemfile
+source 'https://rubygems.org'
+
+gem 'better_errors'
+gem 'binding_of_caller'
+gem 'puma', '~> 5.6.7'
+gem 'rackup', '~> 1.0.1'
+gem 'sinatra', '~> 3.0.6'
+gem 'sinatra-contrib'
---
+++ dice.rb
@@ -0,0 +1,66 @@
+# /dice.rb
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'better_errors'
+require 'binding_of_caller'
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+get('/') do
+  erb(:elaphant)
+end
+
+get('/dice/2/6') do
+  @rolls = []
+
+  2.times do
+    die = rand(1..6)
+    @rolls.push(die)
+  end
+
+  erb(:two_six)
+end
+
+get('/dice/2/10') do
+  @rolls = []
+
+  2.times do
+    die = rand(1..10)
+    @rolls.push(die)
+  end
+
+  erb(:two_ten)
+end
+
+get('/dice/1/20') do
+  @die = rand(1..20)
+  @outcome = "You rolled a #{@die}."
+  erb(:one_twenty)
+end
+
+get('/dice/5/4') do
+  @rolls = []
+
+  5.times do
+    die = rand(1..4)
+    @rolls.push(die)
+  end
+
+  erb(:five_four)
+end
+
+get('/dice/100/6') do
+  @rolls = [] # Create blank array
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
+end
